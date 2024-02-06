9d0981fc025a8a4bd883317403f9e180fabfe8ce
Rossini fonseca <rossinidrum@outlook.com>
2024-02-05 23:58:04 -0300

    feat: adiciona useTaskContext

--- app/src/components/task/NoTask.tsx
+++ app/src/components/task/NoTask.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { useTaskContext } from '~/context/TaskContextProvider';
 
 function NoTask() {
+  const { openCreateTask } = useTaskContext();
+
   return (
-    <View className="items-center w-full justify-center h-full gap-4">
+    <View className="items-center  w-full justify-center h-full gap-4">
       <Image style={{ width: 315, height: 202 }} source={require('~/assets/no-task.png')} />
       <Text className="text-center text-secundary font-medium text-2xl">
         Você não possui nenhuma tarefa criada. Crie um tarefa para começar
       </Text>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={openCreateTask}>
         <View className="w-[230px] h-[60px] bg-primary rounded-lg items-center justify-center">
           <Text className="text-3xl text-white font-medium">Criar</Text>
         </View>
